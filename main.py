ea36c6fe01f9eaed42270eec2a8bfea529ecfbf5
Issac Jefrin <issacjefrin121@gmail.com>
2026-03-19 00:19:45 +0530

    fix missing asgiref

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import uvicorn
 from asgiref.wsgi import WsgiToAsgi
-
 from app import app as flask_app
 
 app = WsgiToAsgi(flask_app)
 
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
